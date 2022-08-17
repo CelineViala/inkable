3d2cc8167e8638519bb54499c4ae87e6b046889a
CelineViala <celin68644@aol.com>
2022-08-17 15:08:28 +0200

    signUpPro

--- back/app/controllers/authController.js
+++ back/app/controllers/authController.js
@@ -1,17 +1,53 @@
 const consumerController = require("./consumerController");
 const proController=require("./proController");
 const jwt = require('jsonwebtoken');
+const emailValidator = require('email-validator')
 
 const authController={
-    signup(req,res){
-        //traitement du formulaire d'inscription lors de la soumission
+    signupPro(req,res){
+        //!TODO
+        //Vérification du formulaire d'inscription
+        const errors = [];
+
+        //Vérification de chaque champs
+        if (!req.body.studio_name || !req.body.email || !req.body.password || !req.body.passwordConfirm || !req.body.couleur || !req.body.black_and_white) {
+        errors.push('Vous devez remplir tous les champs !');
+        }
+
+        //Vérification si l'email n'est pas au bon format
+         if (!emailValidator.validate(req.body.email)) {
+          errors.push("L'email n'est pas valide !");
+        }
+
+        // Vérication que le mot de passe soit bon
+        if (req.body.password !== req.body.passwordConfirm) {
+            errors.push('Les deux mots de passes ne correspondent pas !');
+        }
+
+        // Vérifie si le pro existe déjà ou non dans la BDD
+        const pro = await Pro.getOnePro({
+            where: { 
+                email: req.body.email,
+            },
+        });
+        
+        // si il trouve un pro déjà existant en BDD via l'email, alors c'est que le pro est déjà inscrit !
+        if (user) {
+            errors.push('Un utilisateur inscrit sur le site possède déjà cette adresse email !');
+        }
+
+         // S'il y a des erreurs, on renvoit le formulaire en montrant les erreurs
+         if (errors.length > 0) return res.render('signup', { errors });
     },
+
     testConsumer(req,res){
         res.json("Page autorisée seulement aux consumers");
     },
+
     testPro(req, res){
         res.json("Page autorisée seulement aux pros");
     },
+
     async login(req,res){
        
         const { email, password } = req.body;
@@ -28,17 +64,6 @@ const authController={
                 email: req.body.email
             }
         });
-
-
-        //!TODO
-        //Vérification du formulaire d'inscription
-        // const errors = [];
-
-        //vérification de chaque champs
-        // if (!req.body.studio_name || !req.body.email || !req.body.password || !req.body.passwordConfirm || !req.body.couleur || !req.body.black_and_white) {
-        // errors.push('Vous devez remplir tous les champs !');
-        // }
-
         
         consumer={email:"a@a.com",password:123};
         const accessTokenSecretConsumer = 'accessConsumer';
