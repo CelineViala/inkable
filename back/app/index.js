d29150a2c6a72a0a5683675d250fbd73de6bd21c
CelineViala <celin68644@aol.com>
2022-08-17 17:09:13 +0200

    config sequelize

--- back/app/index.js
+++ back/app/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 const router = require('./routers');
-const router2= require('./routers/routerAuth');
+const routerAuth= require('./routers/routerAuth');
 app.use(express.urlencoded({ extended: true }));
 
 //! penser aux CORS
-app.use(router2);
+app.use(router);
 module.exports = app;
