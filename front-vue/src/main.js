0e4250e0b6b57851189ed6779b68916714b494e4
CelineViala <celin68644@aol.com>
2022-08-22 13:32:07 +0200

    installation bootstrap

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
@@ -10,3 +11,5 @@ createApp(App)
 .use(createStore)
 .use(router)
 .mount('#app');
+
+import "bootstrap/dist/js/bootstrap.js";
