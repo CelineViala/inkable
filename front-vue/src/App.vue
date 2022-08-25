bb56cb902974b8a2a0a0eafe3eb9e044358713cb
Etudiant O'clock <lucasragot1@gmail.com>
2022-08-25 15:38:15 +0200

    header and nav bar done

--- front-vue/src/App.vue
+++ front-vue/src/App.vue
@@ -1,62 +1,94 @@
 <template>
 <h1>{{this.user}}</h1>
-  <nav>
-    <router-link to="/">
-      Accueil
-    </router-link> |
-    
-    <router-link to="/inscriptionPro">
-      Inscriptions Pro
-    </router-link> |
-    <router-link to="/inscriptionConsumer">
-      Inscriptions Consumer
-    </router-link> |
-    <router-link to="/connexion">
-      Connexion
-    </router-link> |
+<!-- 1ere partie du header -->
+<div class="px-3 py-2 text-bg-dark">
+      <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <router-link to="/">
+            <img src="https://res.cloudinary.com/dmoacy4yl/image/upload/c_thumb,w_200,g_face/v1661428752/wni8hsgub8egxjqdfufc.jpg" alt="">
+          </router-link>
 
-    <!-- <router-link to="/dashbord-pro" v-if="this.user==='pro'">
-      DashboardPro
-    </router-link> -->
+          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+            <li>
+              <router-link to="/dashbord-pro" class="text-decoration-none">
+                <div class="nav-link text-white">
+                  Dashboard Pro
+                </div>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/dashbord-particulier" class="text-decoration-none">
+                <div class="nav-link text-white">
+                  Dashboard Part
+                </div>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/planning" class="text-decoration-none">
+                <div class="nav-link text-white">
+                  Planning
+                </div>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/profil-pro/1" class="text-decoration-none">
+                <div class="nav-link text-white">
+                  Profil Pro
+                </div>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/compte-pro" class="text-decoration-none">
+                <div class="nav-link text-white">
+                  Modif Compte Pro
+                </div>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/compte-particulier" class="text-decoration-none">
+                <div class="nav-link text-white">
+                  Modif Compte Part
+                </div>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
 
-    <router-link to="/profil-pro/1">
-       ProfilPro
-    </router-link> |
+    <!-- 2eme partie -->
+    <div class="px-3 py-2 border-bottom mb-3">
 
-    <router-link to="/dashbord-pro">
-      DashboardPro
-    </router-link> |
-    <!-- v-if="this.user==='pro' -->
-  
-    <router-link to="/dashbord-particulier">
-      DashboardParticulier
-    </router-link> |
+        <div class="text-end">
+          <router-link to="/connexion">
+            <button type="button" class="btn btn-light text-dark me-2">Se connecter</button>
+          </router-link>
+          <router-link to="/inscriptionConsumer">
+            <button type="button" class="btn btn-primary me-2">Créer compte part</button>
+          </router-link>
+          <router-link to="/inscriptionPro">
+            <button type="button" class="btn btn-primary">Créer compte pro</button>
+          </router-link> 
+        </div>
 
-    <router-link to="/compte-pro">
-      ComptePro
-    </router-link> |  
+    </div>
 
-    <router-link to="/compte-particulier">
-      CompteParticulier
-    </router-link> |
+<!-- 3eme partie -->
+    <div class="px-3 py-2 border-bottom mb-3">
 
-    <router-link to="/formulaire-project">
-      FormulaireProject
-    </router-link> |
-
-    <router-link to="/project">
-      Project
-    </router-link> |
-
-    <router-link to="/planning">
-      Planning
-    </router-link> |
-
-    <router-link to="/page404">
-      Page404
-    </router-link>
-
-  </nav>
+        <div class="text-end">
+          <router-link to="/page404">
+            <button type="button" class="btn btn-light text-dark me-2">404</button>
+          </router-link>
+          <router-link to="/project">
+            <button type="button" class="btn btn-primary me-2">projet</button>
+          </router-link>
+          <router-link to="/formulaire-project">
+            <button type="button" class="btn btn-primary me-2">formulaire projet</button>
+          </router-link>
+        </div>
+        
+    </div>
   <router-view />
 </template>
 
