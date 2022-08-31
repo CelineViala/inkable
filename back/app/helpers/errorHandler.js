890d52ad949a8e0a80dc15b6182a57cc8e96a3ac
Etudiant O'clock <lucasragot1@gmail.com>
2022-08-31 12:23:51 +0200

    v3

--- back/app/helpers/errorHandler.js
+++ back/app/helpers/errorHandler.js
@@ -3,9 +3,9 @@ const errorHandler = (err, _, res, next) => {
     let { message } = err;
     const { infos } = err;
     const statusCode = infos?.statusCode || 500;
-    if (statusCode === 500 && process.env.NODE_ENV === 'production') {
-        message = 'Internal Server Error';
-    }
+    // if (statusCode === 500 && process.env.NODE_ENV === 'production') {
+    //     message = 'Internal Server Error';
+    // }
     res.status(statusCode).json({
         status: 'error',
         statusCode,
