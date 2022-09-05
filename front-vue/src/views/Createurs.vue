fa6c7bd904e549715fd7d6664ceed20181bc57ad
lauriealarcon <laurie@alarcon.eu>
2022-09-05 13:25:47 +0200

    ajout profil github

--- front-vue/src/views/Createurs.vue
+++ front-vue/src/views/Createurs.vue
@@ -4,12 +4,12 @@
       <!-- Création de la ligne -->
       <div class="row d-flex justify-content-center align-items-center h-100">
         <!-- Première colonne -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6">
           <div
             class="card bg-dark text-white"
             style="border-radius: 1rem;"
           >
-            <div class="card-body p-4 text-center">
+            <div class="card-body  text-center">
               <img
                 src="https://res.cloudinary.com/dmoacy4yl/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1661428752/wni8hsgub8egxjqdfufc.jpg"
                 alt="Generic placeholder image"
@@ -18,27 +18,37 @@
               >
             </div>
 
-            <h3>Céline Viala</h3>
-            <h5 class="text-muted">
-              Git & Vue.js Master
-            </h5>
-            <h5 class="text-muted">
-              Solution finder
-            </h5>
-            <a
-              href="https://www.linkedin.com/in/devcelinev/"
-              target="_blank"
-            > <i class="bi bi-linkedin" /></a>
+            <div class="card-body text-center">
+              <h3>Céline Viala</h3>
+              <h5 class="text-muted">
+                Git & Vue.js Master
+              </h5>
+              <h5 class="text-muted">
+                Solution finder
+              </h5>
+            </div>
+
+            <div class="card-body text-center">
+              <a
+                href="https://www.linkedin.com/in/devcelinev/"
+                target="_blank"
+              > <i class="bi bi-linkedin me-4 " /></a>
+
+              <a
+                href="https://github.com/CelineViala"
+                target="_blank"
+              > <i class="bi bi-github" /></a>
+            </div>
           </div>
         </div>
                 
         <!-- Seconde colonne -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6">
           <div
             class="card bg-dark text-white"
             style="border-radius: 1rem;"
           >
-            <div class="card-body p-4 text-center">
+            <div class="card-body text-center">
               <img
                 src="https://res.cloudinary.com/dmoacy4yl/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1661435375/100404930-closeup-portrait-de-chat-gingembre-dr%C3%B4le-portant-des-lunettes-de-soleil-isol%C3%A9-sur-blanc-mise-au-poin_tdazyq.webp"
                 alt="Generic placeholder image"
@@ -46,24 +56,31 @@
                 style="width: 180px; border-radius: 10px;"
               >
             </div>
-
-            <h3>Lucas Ragot</h3>
-            <h5 class="text-muted">
-              Lead dev Front & Back
-            </h5>
-            <h5 class="text-muted">
-              Framework installer
-            </h5>
+            <div class="card-body text-center">
+              <h3>Lucas Ragot</h3>
+              <h5 class="text-muted">
+                Lead dev Front & Back
+              </h5>
+              <h5 class="text-muted">
+                Framework installer
+              </h5>
+            </div>
+            <div class="card-body text-center">
+              <a
+                href="https://github.com/Lucas-Ragot"
+                target="_blank"
+              > <i class="bi bi-github" /></a>
+            </div>
           </div>
         </div> 
 
         <!-- Troisième colonne -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6 ">
           <div
             class="card bg-dark text-white"
             style="border-radius: 1rem;"
           >
-            <div class="card-body p-4 text-center">
+            <div class="card-body text-center">
               <img
                 src="https://res.cloudinary.com/dmoacy4yl/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1661435540/chat_alfy6e.jpg"
                 alt="Generic placeholder image"
@@ -71,18 +88,26 @@
                 style="width: 180px; border-radius: 10px;"
               >
             </div>
+            <div class="card-body text-center">
+              <h3>Laurie Alarçon</h3>
+              <h5 class="text-muted">
+                Product owner
+              </h5>
+              <h5 class="text-muted">
+                Harmonisation designer
+              </h5>
+            </div>
+            <div class="card-body text-center">
+              <a
+                href="https://www.linkedin.com/in/lauriealarcon/"
+                target="_blank"
+              > <i class="bi bi-linkedin me-4 " /></a>
 
-            <h3>Laurie Alarçon</h3>
-            <h5 class="text-muted">
-              Product owner & Scrum Master
-            </h5>
-            <h5 class="text-muted">
-              Harmonisation designer
-            </h5>
-            <a
-              href="https://www.linkedin.com/in/lauriealarcon/"
-              target="_blank"
-            > <i class="bi bi-linkedin" /></a>
+              <a
+                href="https://github.com/lauriealarcon"
+                target="_blank"
+              > <i class="bi bi-github" /></a>
+            </div>
           </div>
         </div>
       </div>
